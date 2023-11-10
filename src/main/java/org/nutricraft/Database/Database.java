3588d5c0eee248aeb3d13d439d1b283aa87f127e
afnanramadhan <13521011@std.stei.itb.ac.id>
2023-11-10 22:08:00 +0700

    add: databases

--- src/main/java/org/nutricraft/Database/Database.java
+++ src/main/java/org/nutricraft/Database/Database.java
@@ -2,23 +2,29 @@ package org.nutricraft.Database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class Database {
 
-
+    protected Connection connection;
 
     public Database(){
         try{
             String url = System.getenv("DATABASE_URL");
             String username = System.getenv("DB_USER");
             String password = System.getenv("DB_PASSWORD");
-            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("url: " + url);
+            this.connection = DriverManager.getConnection(url, username, password);
 
-            System.out.println("create database");
+            System.out.println("Connected to database");
         }catch (Exception e){
             e.printStackTrace();
-            System.out.println("Something went wrong.....");
+            System.out.println("Something went wrong when connecting to database");
         }
     }
     
+    public Connection getConn(){
+        return connection;
+    }
+
 }

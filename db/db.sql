ab18a4d367c32b699742b39dc09e509650aadde1
afnanramadhan <13521011@std.stei.itb.ac.id>
2023-11-11 18:17:13 +0700

    update sql file

--- db/db.sql
+++ db/db.sql
@@ -1,37 +1,11 @@
--- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
---
--- Host: localhost    Database: nutricraft
--- ------------------------------------------------------
--- Server version	10.6.12-MariaDB
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `coins`
---
 
 DROP TABLE IF EXISTS `coins`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `coins` (
   `id` varchar(255) NOT NULL,
   `coin` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `coins`
---
 
 INSERT INTO `coins` (`id`, `coin`) VALUES
   ('4169868951', 10),
@@ -45,90 +19,63 @@ INSERT INTO `coins` (`id`, `coin`) VALUES
   ('7807240504', 23),
   ('7589780480', 1);
 
-LOCK TABLES `coins` WRITE;
-/*!40000 ALTER TABLE `coins` DISABLE KEYS */;
-/*!40000 ALTER TABLE `coins` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `creatorlevels`
---
-
 DROP TABLE IF EXISTS `creatorlevels`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `creatorlevels` (
   `id` varchar(255) NOT NULL,
   `exp` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `creatorlevels`
---
+INSERT INTO `creatorlevels`(`id`, `exp`) VALUES
+    ('4169868951', 103),
+      ('6621876564', 3459),
+      ('0068131062', 325),
+      ('2534892940', 645),
+      ('2542003092', 745),
+      ('2076048790', 8567),
+      ('7833528552', 7456),
+      ('5858179630', 25456),
+      ('7807240504', 6734),
+      ('7589780480', 1);
 
-LOCK TABLES `creatorlevels` WRITE;
-/*!40000 ALTER TABLE `creatorlevels` DISABLE KEYS */;
-/*!40000 ALTER TABLE `creatorlevels` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `subscribers`
---
 
 DROP TABLE IF EXISTS `subscribers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `subscribers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_user` int(11) NOT NULL,
   `id_creator` varchar(255) NOT NULL,
   PRIMARY KEY (`id`,`id_user`,`id_creator`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `subscriber`
---
+INSERT INTO `subscribers` VALUES
+    (1,3,'4169868951'),
+    (2,1,'6621876564'),
+    (3,3,'0068131062'),
+    (4,3,'2534892940'),
+    (5,4,'2542003092'),
+    (6,2,'2076048790'),
+    (7,4,'7833528552'),
+    (8,2,'5858179630'),
+    (9,2,'7807240504'),
+    (10,2,'7589780480');
 
-LOCK TABLES `subscribers` WRITE;
-/*!40000 ALTER TABLE `subscribers` DISABLE KEYS */;
-INSERT INTO `subscribers` VALUES (1,3,'4169868951'),(2,1,'6621876564'),(3,3,'0068131062'),(4,3,'2534892940'),(5,4,'2542003092'),(6,2,'2076048790'),(7,4,'7833528552'),(8,2,'5858179630'),(9,2,'7807240504'),(10,2,'7589780480');
-/*!40000 ALTER TABLE `subscribers` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `userlevels`
---
 
 DROP TABLE IF EXISTS `userlevels`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `userlevels` (
   `id` int(11) NOT NULL,
   `exp` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `userlevels`
---
+INSERT INTO `userlevels` (`id`, `exp`) VALUES
+    (2,100),
+    (3,7594),
+    (4,32894),
+    (1,72);
 
-LOCK TABLES `userlevels` WRITE;
-/*!40000 ALTER TABLE `userlevels` DISABLE KEYS */;
-/*!40000 ALTER TABLE `userlevels` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
 
 DROP TABLE IF EXISTS `logging`;
 CREATE TABLE `logging`
@@ -142,6 +89,4 @@ CREATE TABLE `logging`
 );
 
 INSERT INTO `logging` (`id`, `description`, `ip`, `endpoint`, `requested_at`) VALUES
-  (1, 'first log', '127.0.0.1', 'localhost.com', '2008-01-01 00:00:01');
-
--- Dump completed on 2023-11-10 20:21:41
+  (1, 'coba log', '127.0.0.1', 'localhost.com', '2008-01-01 00:00:01');
